9290aa8a39b54b936c812ac84e6f5f59977d8b97
Rafael Tapia <rafaelsilvatapia@gmail.com>
2024-08-04 23:06:42 -0300

    refactor: extract interval to own type

--- src/notification/domain/models/rule.model.ts
+++ src/notification/domain/models/rule.model.ts
@@ -1,8 +1,16 @@
+export type RuleInterval =
+  | 'second'
+  | 'minute'
+  | 'hour'
+  | 'day'
+  | 'week'
+  | 'month';
+
 export interface RuleModel {
   id: string;
   type: string;
   limit: number;
-  every: 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month';
+  every: RuleInterval;
   createdAt: Date;
   updatedAt: Date;
 }
