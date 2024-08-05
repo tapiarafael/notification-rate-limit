f8181e9908daa0c221e81c5b702f0c1ed4875fac
Rafael Tapia <rafaelsilvatapia@gmail.com>
2024-08-05 01:21:49 -0300

    feat: validate sendNotification body

--- src/notification/presentation/dtos/send-notification.dto.ts
+++ src/notification/presentation/dtos/send-notification.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class SendNotificationDto {
+  @IsString()
   type: string;
+
+  @IsString()
   userId: string;
+
+  @IsString()
   message: string;
 }
